a65e562f234de2077bece7804851f076fbfd5d3f
waiphyo69 <waiphyo300@gmail.com>
2015-06-14 12:44:40 -0400

    finish blogger tutorial first section, add articles page, create html.erb files for show and index

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'dbbff00480f88bb824cb387ba8d56f94dca1aef1b6be9e25ae9232ca9e4384f801c18720db5d0f0286bf901e3d8774b14725f74671ad963d42653698c641a504'
